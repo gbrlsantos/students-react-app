65e64628c1160d222f3889fe7ffb4f56571528ad
Gabriela Cruz <gabriela.cruz@concilie.com.br>
2022-07-22 01:56:56 -0300

    add student request on form submit

--- src/components/NewStudentForm.tsx
+++ src/components/NewStudentForm.tsx
@@ -1,22 +1,51 @@
-import { InputGroup, InputLeftElement, Stack, Input, Button } from "@chakra-ui/react"
+import { InputGroup, InputLeftElement, Stack, Input, Button, useControllableState } from "@chakra-ui/react"
 import { AtSignIcon } from '@chakra-ui/icons'
+import { useStudentMutation } from "../hooks/useRequest"
+import { createStudent } from "../graphql"
+import { ApolloCache, FetchResult } from "@apollo/client"
+import { IStudentMutation } from "../interfaces/IStudent"
 
 const NewStudentForm: React.FC = () => {
+  const [ addStudent ] = useStudentMutation(createStudent)
+
+  const [ email, setEmail ] = useControllableState({ defaultValue: '' })
+  const [ cpf, setCpf ] = useControllableState({ defaultValue: '' })
+  const [ name, setName ] = useControllableState({ defaultValue: '' })
+
+  function handleSubmitForm() {
+    addStudent({
+      variables: { name, cpf, email }
+    })
+  }
+
   return (
     <Stack spacing={4}>
-      <Input type='text' placeholder='Nome do estudante' />
+      <Input
+        type='text'
+        placeholder='Nome do estudante' 
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) => setName(e.currentTarget.value)}
+      />
       <InputGroup>
         <InputLeftElement
           pointerEvents='none'
           children={<AtSignIcon color='gray.400'/>}
         />
-        <Input type='email' placeholder='E-mail do estudante' />
+        <Input
+          type='email'
+          placeholder='E-mail do estudante'
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => setEmail(e.currentTarget.value)}
+        />
       </InputGroup>
-      <Input type='text' placeholder='CPF do estudante' />
+      <Input
+        type='text'
+        placeholder='CPF do estudante'
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) => setCpf(e.currentTarget.value)}
+      />
       <Button
         marginBottom={"28"}
         backgroundColor={"black"}
         color={"white"}
+        onClick={() => handleSubmitForm()}
       >
         Cadastrar
       </Button>
