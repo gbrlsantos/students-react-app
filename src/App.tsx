acdf1eb6925bcc407c0a2e69da08935e86363874
Gabriela Cruz <gabriela.cruz@concilie.com.br>
2022-07-22 00:18:37 -0300

    cleaning app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,17 @@
 import * as React from "react"
 import {
   ChakraProvider,
-  Box,
-  extendTheme,
   Center,
-  Table,
-  Thead,
-  Tbody,
-  Tfoot,
-  Tr,
-  Th,
-  Td,
-  TableCaption,
-  TableContainer,
 } from "@chakra-ui/react";
 
-import { findStudents } from "./graphql";
-import { useStudentQuery } from "./hooks/useRequest";
-import Student from "./components/Student";
-import { IStudent } from "./interfaces/IStudent";
-
-const theme = extendTheme({
-  colors: {
-    descomplica: {
-      100: "#00E88F",
-    },
-  },
-})
+import theme from "./themes/theme";
+import StudentsTable from "./components/StudentsTable";
 
 const App: React.FC = () => {
-  const { loading, error, data } = useStudentQuery(findStudents);
-  const students = data?.getStudents
-
-  if (error) return <h1>Something went wrong!</h1>;
-
-  if (loading) return <h1>Loading...</h1>;
-
   return (
       <ChakraProvider theme={theme}>
         <Center>
-          <Box borderTopLeftRadius="2rem" borderTopRightRadius="2rem" mt='28' bg="descomplica.100" h="100vh" w="70vw">
-            <TableContainer>
-              <Table variant='unstyled'>
-                <Thead>
-                  <Tr>
-                    <Th>Name</Th>
-                    <Th>E-mail</Th>
-                    <Th>CPF</Th>
-                  </Tr>
-                </Thead>
-                <Tbody>
-                  {students && students.map((student: IStudent) => (
-                    <Student key={student._id} student={student} />
-                  ))}
-                </Tbody>
-              </Table>
-            </TableContainer>
-          </Box>
+          <StudentsTable/>
         </Center>
       </ChakraProvider>
   );
