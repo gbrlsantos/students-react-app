8b274783f1851d04b3b98437d4588385200b561a
Gabriela Cruz <gabriela.cruz@concilie.com.br>
2022-07-22 00:36:38 -0300

    add header with logo and create new student button

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import {
   Center,
   Container,
   Flex,
+  Image,
+  Button,
 } from "@chakra-ui/react";
 
 import theme from "./themes/theme";
@@ -12,9 +14,12 @@ import StudentsTable from "./components/StudentsTable";
 const App: React.FC = () => {
   return (
       <ChakraProvider theme={theme}>
-        <Container maxW="70vw">
-          <Flex>
-
+        <Container maxW="80vw">
+          <Flex justifyContent="space-between" my="8">
+            <Image src="https://d3awytnmmfk53d.cloudfront.net/landings/static/images/core/logo_verde.svg"/>
+            <Button size="md" color={"black"} border="2px" backgroundColor={"transparent"} borderRadius="2rem" fontWeight={"bold"}>
+              Criar novo estudante
+            </Button>
           </Flex>
           <Center>
             <StudentsTable/>
