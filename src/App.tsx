2bf50d006223ce1dec5865c93ee084deda05a67b
Gabriela Cruz <gabriela.cruz@concilie.com.br>
2022-07-22 00:24:43 -0300

    external container with fixed width

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import * as React from "react"
 import {
   ChakraProvider,
   Center,
+  Container,
+  Flex,
 } from "@chakra-ui/react";
 
 import theme from "./themes/theme";
@@ -10,9 +12,14 @@ import StudentsTable from "./components/StudentsTable";
 const App: React.FC = () => {
   return (
       <ChakraProvider theme={theme}>
-        <Center>
-          <StudentsTable/>
-        </Center>
+        <Container maxW="70vw">
+          <Flex>
+
+          </Flex>
+          <Center>
+            <StudentsTable/>
+          </Center>
+        </Container>
       </ChakraProvider>
   );
 };
