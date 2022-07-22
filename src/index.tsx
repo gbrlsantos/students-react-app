3d8d871775bcbcfda9458ee4e72293c607da984d
Gabriela Cruz <gabriela.cruz@concilie.com.br>
2022-07-21 23:40:56 -0300

    add apolloclient server connection

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,21 @@
-import { ColorModeScript } from "@chakra-ui/react"
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client"
 import * as React from "react"
 import * as ReactDOM from "react-dom/client"
-import { App } from "./App"
+import App  from "./App"
 
 const container = document.getElementById("root")
 if (!container) throw new Error('Failed to find the root element');
 const root = ReactDOM.createRoot(container)
 
+const client = new ApolloClient({
+  uri: "http://localhost:4000/",
+  cache: new InMemoryCache(),
+});
+
 root.render(
   <React.StrictMode>
-    <ColorModeScript />
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
 )
