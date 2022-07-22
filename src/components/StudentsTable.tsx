b2f8e7b0b49667e0593bff671c5ca1b819db5a26
Gabriela Cruz <gabriela.cruz@concilie.com.br>
2022-07-22 00:18:24 -0300

    students table as isolated component

---
+++ src/components/StudentsTable.tsx
@@ -0,0 +1,36 @@
+import { Box, Table, TableContainer, Tbody, Th, Thead, Tr } from "@chakra-ui/react";
+import { findStudents } from "../graphql";
+import { useStudentQuery } from "../hooks/useRequest";
+import { IStudent } from "../interfaces/IStudent";
+import LoadingScreen from "./generic/LoadingScreen";
+import Student from "./Student";
+
+const StudentsTable: React.FC = () => {
+  const { loading, error, data } = useStudentQuery(findStudents);
+  if (error) return <h1>Something went wrong!</h1>;
+
+  if (loading) return <LoadingScreen />;
+
+  return(
+    <Box borderTopLeftRadius="2rem" borderTopRightRadius="2rem" mt='28' bg="descomplica.100" h="100vh" w="70vw">
+      <TableContainer>
+        <Table variant='unstyled'>
+          <Thead>
+            <Tr>
+              <Th>Nome</Th>
+              <Th>E-mail</Th>
+              <Th>CPF</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {data?.findStudents.map((student: IStudent) => (
+              <Student key={student._id} student={student} />
+            ))}
+          </Tbody>
+        </Table>
+      </TableContainer>
+    </Box>
+  )
+}
+
+export default StudentsTable;
