f825b914a9ed1444ef98d20e56eebd48a04fd9e1
Gabriela Cruz <gabrielasnts30@gmail.com>
2022-07-22 11:25:26 -0300

    same modal to display create or update student

--- src/components/NewStudentModal.tsx
+++ src/components/NewStudentModal.tsx
@@ -1,22 +1,29 @@
 import { Modal, ModalBody, ModalCloseButton, ModalContent, ModalHeader, ModalOverlay } from "@chakra-ui/react";
 
 import React from "react";
+import { IStudent } from "../interfaces/IStudent";
 import NewStudentForm from "./NewStudentForm";
 
 type Props = {
   isModalOpen: boolean
   setIsModalOpen: React.Dispatch<React.SetStateAction<boolean>>
+  newStudent: boolean
+  student?: IStudent
 };
 
-const NewStudentModal: React.FC<Props> = ({isModalOpen, setIsModalOpen}) => {
+const NewStudentModal: React.FC<Props> = ({isModalOpen, setIsModalOpen, newStudent, student}) => {
   return(
     <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} size={"2xl"}>
       <ModalOverlay />
       <ModalContent>
-        <ModalHeader>Criar novo estudante</ModalHeader>
+        <ModalHeader>
+          { newStudent && "Criar novo estudante" }
+          { student && `Editar ${student.name}` }
+        </ModalHeader>
         <ModalCloseButton />
         <ModalBody>
-          <NewStudentForm />
+          { newStudent && <NewStudentForm newStudent={true} /> }
+          { student && <NewStudentForm newStudent={false} student={student} /> }
         </ModalBody>
       </ModalContent>
     </Modal>
