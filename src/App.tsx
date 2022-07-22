b426433d2eb317159cdfec01288269e47fcecbf9
Gabriela Cruz <gabriela.cruz@concilie.com.br>
2022-07-22 00:58:33 -0300

    header component import on app

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,17 @@ import {
   ChakraProvider,
   Center,
   Container,
-  Flex,
-  Image,
-  Button,
 } from "@chakra-ui/react";
 
 import theme from "./themes/theme";
 import StudentsTable from "./components/StudentsTable";
+import Header from "./components/Header";
 
 const App: React.FC = () => {
   return (
       <ChakraProvider theme={theme}>
         <Container maxW="80vw">
-          <Flex justifyContent="space-between" my="8">
-            <Image src="https://d3awytnmmfk53d.cloudfront.net/landings/static/images/core/logo_verde.svg"/>
-            <Button size="md" color={"black"} border="2px" backgroundColor={"transparent"} borderRadius="2rem" fontWeight={"bold"}>
-              Criar novo estudante
-            </Button>
-          </Flex>
+          <Header/>
           <Center>
             <StudentsTable/>
           </Center>
