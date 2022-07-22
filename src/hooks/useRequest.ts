3f80f8af6a728c65d99c2f70032eb9a57913028e
Gabriela Cruz <gabrielasnts30@gmail.com>
2022-07-22 14:17:59 -0300

    passing refetch as props in usestudentquery

--- src/hooks/useRequest.ts
+++ src/hooks/useRequest.ts
@@ -2,8 +2,8 @@ import { DocumentNode, useQuery, useMutation } from "@apollo/react-hooks";
 import { IStudents, IStudentMutation } from "../interfaces/IStudent";
 
 export function useStudentQuery(gqlQuery: DocumentNode) {
-  const { loading, error, data } = useQuery<IStudents>(gqlQuery);
-  return { loading, error, data };
+  const { loading, error, data, refetch } = useQuery<IStudents>(gqlQuery);
+  return { loading, error, data, refetch };
 }
 
 export function useStudentMutation(gqlQuery: DocumentNode) {
