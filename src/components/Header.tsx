430f587cfe48ab71ec695958a51fd4e730fa0ff2
Gabriela Cruz <gabriela.cruz@concilie.com.br>
2022-07-22 00:58:57 -0300

    header as isolated component + add modal trigger

---
+++ src/components/Header.tsx
@@ -0,0 +1,26 @@
+import { Button, Flex, Image, useControllableState } from "@chakra-ui/react";
+import NewStudentModal from "./NewStudentModal";
+
+const Header: React.FC = () => {
+  const [ isModalOpen, setIsModalOpen ] = useControllableState({ defaultValue: false })
+
+  return (
+    <Flex justifyContent="space-between" my="8">
+      <Image src="https://d3awytnmmfk53d.cloudfront.net/landings/static/images/core/logo_verde.svg"/>
+      <Button
+        onClick={() => setIsModalOpen(true)}
+        size="md"
+        color={"black"}
+        border="2px"
+        backgroundColor={"transparent"}
+        borderRadius="2rem"
+        fontWeight={"bold"}
+      >
+        Criar novo estudante
+      </Button>
+      { isModalOpen && <NewStudentModal isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen}/>}
+    </Flex>
+  )
+}
+
+export default Header;
