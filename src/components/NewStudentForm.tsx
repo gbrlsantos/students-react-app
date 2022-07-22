92f2971487faa77223cdddb40632cbbbc005bca1
Gabriela Cruz <gabrielasnts30@gmail.com>
2022-07-22 11:25:39 -0300

    add same form to create or update student

--- src/components/NewStudentForm.tsx
+++ src/components/NewStudentForm.tsx
@@ -1,40 +1,62 @@
-import { InputGroup, InputLeftElement, Stack, Input, Button, useControllableState } from "@chakra-ui/react"
+import { InputGroup, InputLeftElement, Stack, Input, Button, useControllableState, Editable, EditablePreview, EditableInput } from "@chakra-ui/react"
 import { AtSignIcon } from '@chakra-ui/icons'
 import { useStudentMutation } from "../hooks/useRequest"
-import { createStudent } from "../graphql"
+import { createStudent, updateStudent } from "../graphql"
+import { IStudent } from "../interfaces/IStudent"
+import { InputHTMLAttributes } from "react"
 
-const NewStudentForm: React.FC = () => {
+type Props = {
+  newStudent: boolean
+  student?: IStudent
+}
+
+const NewStudentForm: React.FC<Props> = ({student, newStudent}) => {
   const [ addStudent ] = useStudentMutation(createStudent)
+  const [ alterStudent ] = useStudentMutation(updateStudent)
 
-  const [ email, setEmail ] = useControllableState({ defaultValue: '' })
-  const [ cpf, setCpf ] = useControllableState({ defaultValue: '' })
-  const [ name, setName ] = useControllableState({ defaultValue: '' })
+  const [ email, setEmail ] = useControllableState({ defaultValue: student? student.email : '' })
+  const [ cpf, setCpf ] = useControllableState({ defaultValue: student? student.cpf : '' })
+  const [ name, setName ] = useControllableState({ defaultValue: student? student.name : '' })
 
   function handleSubmitForm() {
-    addStudent({
-      variables: { name, cpf, email }
-    })
+    if (newStudent) {
+      addStudent({
+        variables: { name, cpf, email }
+      })
+    }
+    else {
+      if (student) {
+        console.log(name)
+        const id = student._id
+        alterStudent({
+          variables: { id: student._id, name}
+        })
+      }
+    }
   }
 
   return (
     <Stack spacing={4}>
       <Input
-        type='text'
-        placeholder='Nome do estudante' 
-        onChange={(e: React.ChangeEvent<HTMLInputElement>) => setName(e.currentTarget.value)}
-      />
+          value={ name }
+          type='text'
+          placeholder='Nome do estudante'
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => setName(e.currentTarget.value)}
+        />
       <InputGroup>
         <InputLeftElement
           pointerEvents='none'
           children={<AtSignIcon color='gray.400'/>}
         />
         <Input
+          value={ email }
           type='email'
           placeholder='E-mail do estudante'
           onChange={(e: React.ChangeEvent<HTMLInputElement>) => setEmail(e.currentTarget.value)}
         />
       </InputGroup>
       <Input
+        value = { cpf }
         type='text'
         placeholder='CPF do estudante'
         onChange={(e: React.ChangeEvent<HTMLInputElement>) => setCpf(e.currentTarget.value)}
@@ -45,7 +67,7 @@ const NewStudentForm: React.FC = () => {
         color={"white"}
         onClick={() => handleSubmitForm()}
       >
-        Cadastrar
+        { newStudent? "Cadastrar" : "Confirmar" }
       </Button>
     </Stack>
   )
